aef6eb8c5baf5b989fc9b0eb5b05a7af9fed966b
Jetch123 <jetchells1995@gmail.com>
2022-12-09 14:46:39 +0100

    Add project files.

---
+++ Assets/Scripts/ControllerCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCamera : MonoBehaviour
+{
+	public GameObject player; //player represents the Player who is the ball
+    private Vector3 offset; 
+    void Start () {
+         offset = transform.position - player.transform.position;
+ //we start by recovering the initial distance between the Camera and the Player
+    }
+    void LateUpdate () {
+         transform.position = player.transform.position + offset;
+ //The Camera will always keep the same distance from the Player
+    }
+
+
+}
